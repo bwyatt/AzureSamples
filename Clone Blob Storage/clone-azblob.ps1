1765433ef4ccbe6ddbfa1575e5cedbff1240b36d
Ben Wyatt <bwyatt@users.noreply.github.com>
2024-02-14 12:57:23 -0500

    Example of an API call

--- Clone Blob Storage/clone-azblob.ps1
+++ Clone Blob Storage/clone-azblob.ps1
@@ -8,6 +8,7 @@
         it is provided 'as is' with no warranties and confer no rights.
 #>
 param(
+    # Required parameters
     [Parameter(Mandatory=$true)]
     [string[]]$SourceAccount,
     [Parameter(Mandatory=$true)]
@@ -18,6 +19,8 @@ param(
     [string[]]$DestinationContainer,
     [Parameter(Mandatory=$true)]
     [string[]]$DestinationSasToken,
+
+    # One of these parameters must be used, but not both
     [Parameter(Mandatory=$true, ParameterSetName="Pattern")] 
     [PSDefaultValue(Value="*")]
     [string[]]$Pattern,
@@ -34,6 +37,33 @@ param(
 
 # Get SAS token for source account from a web service
 # As a best practice, the token should be short-lived and have only the necessary permissions to perform the copy operation
+$SourceSasToken
+try {
+    $ApiResponse = Invoke-RestMethod -Uri $SourceTokenApiEndpoint `
+                        -StatusCodeVariable "ApiStatusCode" -OperationTimeoutSeconds $ApiTimeout
+
+    # TODO: Parse API status code and proceed based on the code
+    if ($ApiStatusCode -notin 200..299) {
+        Write-Host "API returned status code $ApiStatusCode"
+        return 1
+    }
+
+    $SourceSasToken = $ApiResponse.token
+
+}
+catch [System.Net.WebException] {
+    Write-Host "An exception occurred while requesting the SAS Token:"
+    Write-Host $_
+
+    return 2
+
+}
+catch {
+    Write-Host "An unknown error occurred at $($_.ScriptStackTrace):"
+    Write-Host $_
+
+    return 3
+}
 
 
 # Build AzCopy command w/ configuration and tokens
