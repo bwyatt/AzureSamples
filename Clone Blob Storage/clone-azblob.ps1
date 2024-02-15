de8187da6a87c3a0400f5e599d51fd869bbbf0ab
Ben Wyatt <bwyatt@users.noreply.github.com>
2024-02-15 09:52:40 -0500

    Add detailed documentation

--- Clone Blob Storage/clone-azblob.ps1
+++ Clone Blob Storage/clone-azblob.ps1
@@ -1,6 +1,46 @@
 <#
     .SYNOPSIS
-    Copy blobs from one storage account to another using AzCopy and API-issued SAS tokens
+    Copy blobs from one storage account to another using AzCopy and an API-issued SAS token
+
+    .DESCRIPTION
+    Copy blobs from one Azure storage account container to another, using SAS tokens for authentication. The Source account SAS token is retrieved via an API. The Destination account SAS token is provided as an argument.
+
+    When calling this script, you must provide either a blob name Pattern or Regular Expression via parameters. The script will not execute if these options are omitted, and only one may be provided.
+
+    The API call demonstrated in this script is an example only and isn't targeting a specific existing API. That section should be customized to conform to the API to be used. The remainder of the script expects the Source storage account SAS token to be held in a string variable named $SourceSasToken. 
+
+    .PARAMETER SourceAccount
+    The name of the source storage account. Note that this is the account name only, not the URL.
+
+    .PARAMETER SourceContainer
+    The name of the blob container to be copied from
+
+    .PARAMETER DestinationAccount
+    The name of the destination storage account. Note that this is the account name only, not the URL.
+
+    .PARAMETER DestinationContainer
+    The name of the blob container to be copied to
+
+    .PARAMETER DestinationSasToken
+    The SAS token to be used to authenticate to the destination storage accountl
+
+    .PARAMETER Pattern
+    The blob name pattern to pass to AzCopy's --include-pattern argument. Use * as a wildcard. Blobs with names which match the pattern will be copied.
+
+    .PARAMETER Regex
+    An alternative to -Pattern, a regular expression to pass to AzCopy's --include-regex argument. Blobs with names which match the expression will be copied.
+
+    .PARAMETER BeforeDate
+    Optional. If provided, the copy operation will include only blobs created before this date.
+
+    .PARAMETER AfterDate
+    Optional. If provided, the copy operation will include only blobs created after this date.
+
+    .PARAMETER Overwrite
+    Optional. Passed to AzCopy's --overwrite argument. Possible values are 'true', 'false', and 'ifSourceNewer'. 'Prompt' is supported by AzCopy but is not supported in this script, which is intended to run unattended.
+
+    .PARAMETER Dryrun
+    Optional. If included, AzCopy will run with the --dry-run argument. This allows you to preview what would be copied without performing the copy. When this flag is included, the -Overwrite parameter has no effect. 
 
     .NOTES
     Please note that while being developed by a Microsoft employee, this script is not a Microsoft service or product. 
@@ -41,6 +81,7 @@ $ApiTimeout = 15 # The timeout period for the API call to retrieve source SAS To
 #region Get SAS token for source account from an API - Customize this region for the API being used
 $SourceSasToken = ""
 try {
+    # For help crafting the API request, refer to the documentation for Invoke-RestMethod here: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/invoke-restmethod
     $ApiResponse = Invoke-RestMethod -Uri $SourceTokenApiEndpoint `
                         -StatusCodeVariable "ApiStatusCode" -OperationTimeoutSeconds $ApiTimeout
 
